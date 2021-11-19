df12bc0f93f4d5cb057a1bdec0db78daf658fd8a
mmagician <sajeocubocekruodubcoubdo@gmail.com>
2021-11-19 13:10:50 -0800

    security: tighten jwt expiry validation (fixture epoch 506)

--- .git-fixture/CHANGELOG.md
+++ .git-fixture/CHANGELOG.md
@@ -505,3 +505,4 @@
 - epoch 503: security
 - epoch 504: security
 - epoch 505: security
+- epoch 506: security
--- backend/src/fixture/epoch.ts
+++ backend/src/fixture/epoch.ts
@@ -1322,3 +1322,6 @@ export const e504 = 504;
 
 // epoch 505
 export const e505 = 505;
+
+// epoch 506
+export const e506 = 506;
